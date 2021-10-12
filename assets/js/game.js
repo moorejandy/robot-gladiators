4d6ac68c9b45d90461bcf72f262f1015942f97ac
Andy Moore <moorejandy@gmail.com>
2021-10-12 08:40:45 -0400

    add conditions for exiting game

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,10 +68,13 @@ var fight = function(enemyName) {
      
    
      for (var i = 0; i < enemyNames.length; i++) {
+       if(playerHealth > 0 ) {
+        window.alert("Welcome to Robot Galdiators! Round " + (i+1));
         var pickedEnemyName = enemyNames[i];
         enemyHealth = 50;
         fight(pickedEnemyName);
+      } else{
+        window.alert("You have lost your robot in battle. Game Over!");
+      }
     }
 
-    
-// fight ()
