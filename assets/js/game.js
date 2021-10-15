e9ad14217f14b702b67e02b8404b64de422ea982
Andy Moore <moorejandy@gmail.com>
2021-10-15 11:44:44 -0400

    numeric input for shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -180,22 +180,20 @@ while (name==="" || name === null){
   };
 
   var shop = function () {
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE"
-    + " the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
+    
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     switch(shopOptionPrompt){
-      case "refill":
-        case "REFILL":
+      case 1:
         playerInfo.refillHealth();
         break;
 
-        case "upgrade":
-          case "UPGRADE":
+        case 2:
           playerInfo.upgradeAttack();
           break;
 
-          case "leave":
-            case "LEAVE":
+          case 3:
             window.alert("Now leaving the store.");
             break;
 
